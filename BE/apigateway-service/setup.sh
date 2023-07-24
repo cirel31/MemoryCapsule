8dd22b7c066988f956505a977453f10f31bc4d87
yimo22 <yimo22@naver.com>
2023-07-24 15:35:24 +0900

    [S09P11A608-8] Feat: 테스트커밋 ♥ #3

--- BE/apigateway-service/setup.sh
+++ BE/apigateway-service/setup.sh
@@ -12,7 +12,7 @@ container_name="$name-service"
 image_name="$name-image"
 tag_name="latest"
 # env file import
-run_option="-dp 8000:8000 --env-file=/home/ubuntu/setup.env"
+run_option="-dp 8000:8000 --env-file=/var/jenkins_home/workspace/setup.env"
 
 
 stop_container_command="sudo docker stop $container_name"
