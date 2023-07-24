79acd6a01612f2382decd72c5dea31da81f72a98
yimo22 <yimo22@naver.com>
2023-07-24 17:53:07 +0900

    [S09P11A608-8] init: user-service 시작

---
+++ BE/user-service/src/main/java/com/example/userservice/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/health-check")
+    public String getHealth(){
+        return "Hello user-service";
+    }
+}
---
+++ BE/user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+}
