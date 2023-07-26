ab8705f903f7ad81c8c0a25a15f56d2d7bb8550c
leejm9704 <leejm9704@naver.com>
2023-07-26 16:22:28 +0900

    [S09P12A608-235] Feat: 친구 추가 및 삭제관련 로직 처리 (front)

--- FE/src/components/friend/FriendInfo.jsx
+++ FE/src/components/friend/FriendInfo.jsx
@@ -1,11 +1,26 @@
 const FriendInfo = ({id, name, username, email, address, phone, website}) => {
     console.log(id, name, username);
+
+    const addFriend = (e) => {
+        const {value} = e.target;
+        console.log(value);
+    }
+    const discardFriend = (e) => {
+        const {value} = e.target;
+        console.log(value);
+    }
+
     return (
         <div className="FriendItem">
             <div className="info">
-            <span className="author_info">
-              | email : {email} | name : {name} | username : {username}
-            </span>
+                <span className="author_info">
+                  | email : {email} | name : {name} | username : {username}
+                  {
+                    (1===1)
+                    ? <button className="addFriend" value={email} onClick={addFriend}> 친구 추가 </button>
+                    : <button className="discardFriend" value={email} onClick={discardFriend}> 친구 삭제 </button>
+                  }
+                </span>
             </div>
         </div>
     )
