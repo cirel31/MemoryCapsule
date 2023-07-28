5960de2bf56e4e1942e32f142e108548e96d857c
yimo22 <yimo22@naver.com>
2023-07-28 17:46:09 +0900

    [S09P11A608] Feat: 테스트커밋 ♥ #10

--- BE/user-service/setup.sh
+++ BE/user-service/setup.sh
@@ -12,7 +12,7 @@ container_name="$name-service"
 image_name="$name-image"
 tag_name="latest"
 # env file import
-run_option="-d --net santa --env-file=/jenkins/workspace/setup.env"
+run_option="-d --net santa --env-file=/var/jenkins_home/workspace/setup.env"
 
 if [ ! "$(docker ps -a -q -f name=$container_name)" ]; then
     ############################################################
