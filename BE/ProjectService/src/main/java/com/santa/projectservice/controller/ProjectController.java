a634f68b34d685cbc63dcd675b7adb0795af5fe3
JaeHyoNi <kimqhahqhah@naver.com>
2023-08-03 00:17:20 +0900

    [S09P12A608-251] Feat: Swagger 설정
    ProjectService Swagger구현

--- BE/ProjectService/src/main/java/com/santa/projectservice/controller/ProjectController.java
+++ BE/ProjectService/src/main/java/com/santa/projectservice/controller/ProjectController.java
@@ -1,20 +1,24 @@
     package com.santa.projectservice.controller;
 
-    import com.fasterxml.jackson.core.JsonProcessingException;
-    import com.fasterxml.jackson.databind.ObjectMapper;
     import com.santa.projectservice.dto.ArticleDto;
     import com.santa.projectservice.dto.ProjectDto;
     import com.santa.projectservice.dto.RegisterDto;
-    import com.santa.projectservice.dto.UserDto;
     import com.santa.projectservice.exception.User.UserNotFoundException;
     import com.santa.projectservice.exception.article.ArticleProjectNotFoundException;
     import com.santa.projectservice.exception.project.ProjectNotFoundException;
     import com.santa.projectservice.exception.project.ProjectNotFullfillException;
     import com.santa.projectservice.exception.register.RegisterMakeException;
-    import com.santa.projectservice.jpa.Article;
     import com.santa.projectservice.service.ArticleService;
     import com.santa.projectservice.service.ProjectService;
     import com.santa.projectservice.service.UserService;
+    import io.swagger.v3.oas.annotations.Operation;
+    import io.swagger.v3.oas.annotations.Parameter;
+    import io.swagger.v3.oas.annotations.Parameters;
+    import io.swagger.v3.oas.annotations.enums.ParameterIn;
+    import io.swagger.v3.oas.annotations.media.Content;
+    import io.swagger.v3.oas.annotations.media.Schema;
+    import io.swagger.v3.oas.annotations.responses.ApiResponse;
+    import io.swagger.v3.oas.annotations.responses.ApiResponses;
     import lombok.extern.slf4j.Slf4j;
     import org.springframework.http.HttpStatus;
     import org.springframework.http.ResponseEntity;
@@ -23,7 +27,6 @@
     import javax.servlet.http.HttpServletRequest;
     import java.io.IOException;
     import java.text.ParseException;
-    import java.text.SimpleDateFormat;
     import java.time.LocalDateTime;
     import java.time.ZoneId;
     import java.time.format.DateTimeFormatter;
@@ -46,18 +49,46 @@
             this.projectService = projectService;
             this.articleService = articleService;
         }
+        @Operation(summary = "모든 프로젝트 정보를 가져옵니다.")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "성공적으로 모든 프로젝트 정보를 가져옴",
+                        content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ProjectDto.class))
+                })
+        })
         @GetMapping("/all")
         public ResponseEntity<List<ProjectDto>> getAllProject(){
             return ResponseEntity.status(HttpStatus.OK).body(projectService.getAll());
         }
 
+        @Operation(summary = "프로젝트 정보를 ID로 가져옵니다.")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "성공적으로 프로젝트 정보를 가져옴",
+                        content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ProjectDto.class)) }
+                ),
+                @ApiResponse(responseCode = "404", description = "해당 ID를 가진 프로젝트를 찾을 수 없음", content = @Content) }
+        )
         @GetMapping("/root/{projectid}")
         public ResponseEntity<ProjectDto> getProjectByRoot(@PathVariable Integer projectid) throws ProjectNotFoundException {
             Long projectId = Long.valueOf(projectid.toString());
             ProjectDto projectDto = projectService.findProjectByProjectId(projectId);
             return ResponseEntity.status(HttpStatus.OK).body(projectDto);
         }
-
+        @Operation(summary = "id로 프로젝트 정보 가져옵니다. ")
+        @Parameters({
+                @Parameter(
+                        in = ParameterIn.HEADER,
+                        name = "유저의 Id, 토큰이 들어갑니다",
+                        description = "userId",
+                        required = true,
+                        schema = @Schema(type = "Integer")
+                )
+        })
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "성공적으로 프로젝트 정보를 가져옴",
+                        content = { @Content(mediaType = "application/json", schema = @Schema(implementation = ProjectDto.class)) }
+                ),
+                @ApiResponse(responseCode = "404", description = "해당 ID를 가진 프로젝트를 찾을 수 없음", content = @Content) }
+        )
         @GetMapping("/{projectid}")
         public ResponseEntity<ProjectDto> getProjectById(@PathVariable Integer projectid, HttpServletRequest request) throws UserNotFoundException, ProjectNotFoundException {
             Long userId, projectId;
@@ -71,22 +102,12 @@
             }
         }
 
-    //    @GetMapping("/user/{userId}")
-    //    public ResponseEntity<UserDto> getUser(@PathVariable("userId") Long userId) {
-    //        try {
-    //            log.info(userService.findUserById(userId).toString());
-    //            return ResponseEntity.status(HttpStatus.OK).body(userService.findUserById(userId));
-    //        } catch (UserNotFoundException e) {
-    //            log.info(e.getMessage());
-    //            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(null);
-    //        }
-    //    }
 
-        /**
-         * @param httpRequest - header에서 아이디 가져옵니다
-         * @return - 새로 만들어진 프로젝트의 ID
-         * @throws JsonProcessingException - project정보가 없을 떄
-         */
+        @Operation(summary = "새로운 프로젝트를 생성합니다.")
+        @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 프로젝트 생성", content = { @Content(mediaType = "application/json") }),
+            @ApiResponse(responseCode = "400", description = "title 혹은 content가 비어있습니다", content = @Content) }
+        )
         @PostMapping("/create")
         public ResponseEntity<Object> createProject(HttpServletRequest httpRequest, @RequestBody Map<String, Object> map) throws RegisterMakeException, ParseException {
             List<Integer> list = (ArrayList<Integer>) map.get("userList");
@@ -118,6 +139,12 @@
             return ResponseEntity.status(HttpStatus.OK).body(projectId);
         }
 
+
+        @Operation(summary = "프로젝트를 삭제합니다.")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "202", description = "프로젝트 삭제 성공", content = { @Content(mediaType = "application/json") }),
+                @ApiResponse(responseCode = "404", description = "프로젝트를 찾을 수 없음", content = @Content)
+        })
         @DeleteMapping("/{projectid}")
         public ResponseEntity<String> deleteProject(HttpServletRequest request, @PathVariable Integer projectid) throws ProjectNotFoundException {
             Long userId = Long.valueOf(String.valueOf(request.getHeader("userId")));
@@ -127,6 +154,12 @@
             return ResponseEntity.status(HttpStatus.ACCEPTED).body("[" + title + "] : 삭제성공");
         }
 
+
+        @Operation(summary = "사용자 ID로 등록 정보를 가져옵니다.")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "성공적으로 등록 정보를 가져옴",
+                        content = { @Content(mediaType = "application/json",
+                                schema = @Schema(implementation = RegisterDto.class)) })})
         @GetMapping("/list/{userId}")
         public ResponseEntity<List<RegisterDto>> getRegisterById(@PathVariable("userId") Long userId) {
             List<RegisterDto> registerDtoList = projectService.findRegistersByUserId(userId);
@@ -134,6 +167,10 @@
             return ResponseEntity.status(HttpStatus.OK).body(registerDtoList);
         }
 
+        @Operation(summary = "프로젝트 내용을 수정합니다.")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "성공적으로 프로젝트 내용 수정",
+                        content = { @Content(mediaType = "application/json") })})
         @PutMapping("/{projectId}")
         public ResponseEntity<Boolean> editProjectContent(@PathVariable("projectId") Long projectId, @RequestBody String content, HttpServletRequest request) {
             Long id = Long.valueOf(String.valueOf(request.getHeader("userId")));
@@ -141,6 +178,10 @@
             return ResponseEntity.status(HttpStatus.OK).body(check);
         }
 
+        @Operation(summary = "글을 작성합니다.")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "201", description = "성공적으로 글 작성", content = { @Content(mediaType = "application/json") })}
+        )
         @PostMapping("/article/write/{projectId}")
         public ResponseEntity<Boolean> writeArticle(@PathVariable("projectId") Long projectId,
                                                     @RequestBody Map<String, String> map,
@@ -159,6 +200,12 @@
             return ResponseEntity.status(HttpStatus.CREATED).body(result);
         }
 
+
+        @Operation(summary = "프로젝트의 모든 글을 가져옵니다.")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "성공적으로 모든 글을 가져옴",
+                        content = { @Content(mediaType = "application/json",
+                                schema = @Schema(implementation = ArticleDto.class)) })})
         @GetMapping("/{projectid}/article")
         public ResponseEntity<List<ArticleDto>> getProjectArticles(@PathVariable("projectid") Integer projectid,
                                                              HttpServletRequest request){
@@ -168,6 +215,11 @@
             return ResponseEntity.status(HttpStatus.OK).body(articleDtos);
         }
 
+        @Operation(summary = "최근에 작성된 글을 가져옵니다.")
+        @ApiResponses(value = {
+                @ApiResponse(responseCode = "200", description = "성공적으로 최근에 작성된 글을 가져옴",
+                        content = { @Content(mediaType = "application/json",
+                                schema = @Schema(implementation = ArticleDto.class)) })})
         @GetMapping("/{projectid}/latestarticle")
         public ResponseEntity<ArticleDto> getRecentArticles(@PathVariable("projectid") Integer projectid,
                                                                   HttpServletRequest request) throws ArticleProjectNotFoundException {
