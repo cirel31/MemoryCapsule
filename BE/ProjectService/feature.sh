814c3771c871f86bb04eca652df3f13e74cb00df
JaeHyoNi <kimqhahqhah@naver.com>
2023-08-02 13:03:46 +0900

    [S09P12A608-340] Feat: 스크립트 수정

--- BE/ProjectService/feature.sh
+++ BE/ProjectService/feature.sh
@@ -3,15 +3,13 @@ container_name="$name-service"
 image_name="$name-image"
 tag_name="latest"
 profile="dev"
-# env file import
-#run_option="-d --env-file=/var/jenkins_home/workspace/setup.env"
 
 
 stop_container_command="sudo docker stop $container_name"
 remove_container_command="sudo docker rm $container_name"
 remove_image_command="sudo docker rmi $image_name"
 
-if [ ! "$(docker ps -a -q -f name=$container_name)" ]; then
+if [ "$(docker ps -a -q -f name=$container_name)" ]; then
     ############################################################
     # [stop container by name or container-id]
     ############################################################
