b490a34a5dddc015b367581588e6c31dbadca008
JaeHyoNi <kimqhahqhah@naver.com>
2023-08-03 00:17:20 +0900

    [S09P12A608-251] Feat: Swagger 설정
    ProjectService Swagger구현

--- BE/ProjectService/src/main/java/com/santa/projectservice/config/SwaggerConfig.java
+++ BE/ProjectService/src/main/java/com/santa/projectservice/config/SwaggerConfig.java
@@ -1,23 +1,38 @@
 package com.santa.projectservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PROJECT - DEV - API")
+                        .version("1.0.0")
+                        .description("뭐야뭐야")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("Testing")
+                .pathsToMatch("/project/**")
                 .build();
     }
+//
+//    @Bean
+//    public GroupedOpenApi privateApi() {
+//        return GroupedOpenApi.builder()
+//                .setGroup("Private")
+//                .pathsToMatch("/private/**")
+//                .build();
+//    }
 }
