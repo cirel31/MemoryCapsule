b14f86102ca878e5314ff5bf54594d288f004695
TAEGYEONG KIM <uh60blackhawk2@gmail.com>
2023-08-04 16:58:02 +0900

    [S09P12A608-298] Design: SCSS 파일 작성
    회원가입 페이지 제작 완료. (레이아웃 건들지 말 것)

--- FE/src/pages/auth/SignupPage.jsx
+++ FE/src/pages/auth/SignupPage.jsx
@@ -1,6 +1,7 @@
 import SignupForm from "../../components/auth/SignupForm"
 import {useState} from "react";
-
+import signup_bg from "../../assets/images/signup/Sign_up.svg"
+import "../../styles/SignUpPage.scss"
 const SignupPage = () => {
   const [form, setForm] = useState({
     id: "",
@@ -15,7 +16,8 @@ const SignupPage = () => {
 
   return (
     <>
-      <div>
+      <div className="signup_body">
+        <img src={signup_bg} className="signup_back"/>
         <SignupForm form={form} setForm={setForm} onChange={handleChange} />
       </div>
     </>
