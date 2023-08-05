824b7df85d1fce59dc2a358dd9568b7383d9dd4e
JaeHyoNi <kimqhahqhah@naver.com>
2023-08-06 02:39:13 +0900

    [S09P12A608-340] Feat: QueryDSL적용
    이름함수 인터페이스 제거

--- BE/ProjectService/src/main/java/com/santa/projectservice/repository/RegisterRepository.java
+++ BE/ProjectService/src/main/java/com/santa/projectservice/repository/RegisterRepository.java
@@ -10,14 +10,6 @@ import java.util.Optional;
 
 public interface RegisterRepository extends JpaRepository<Register, Long> {
     List<Register> findRegistersByUser_Id(Long id);
-    @Query("SELECT r FROM Register r WHERE r.user.id = :userId AND " +
-            "(CASE WHEN :state = 'ALL' THEN true " +
-            "WHEN :state = 'DONE' AND r.confirm = true THEN true " +
-            "WHEN :state = 'CURRENT' AND r.confirm = false THEN true " +
-            "ELSE false END) = true")
-    List<Register> findByUserIdAndProjectState(@Param("userId") Long userId, @Param("state") String state);
-
-
     /* N+1문제가 터져서 쿼리 직접 작성 */
     @Query("select r.project.id, r.confirm " +
             "from Register r " +
@@ -26,9 +18,6 @@ public interface RegisterRepository extends JpaRepository<Register, Long> {
             "  and r.type = true ")
     Optional<Long> getOwnerRegisterByUserIdAndProjectId(@Param("userId") Long userId, @Param("projectId") Long projectId);
 
-
-
-    Optional<Register> findByUser_IdAndProject_Id(Long userId, Long projectId);
 
     Long countByUser_Id(Long userId);
 }
