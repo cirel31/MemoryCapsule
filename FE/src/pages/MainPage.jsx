7820b9cbd010149cfc32fa2f0a3fc8a4fe97d7ae
TAEGYEONG KIM <uh60blackhawk2@gmail.com>
2023-08-09 17:53:09 +0900

    [S09P12A608-444] Design: 프로젝트 생성 페이지 구현완료 및 잡다한 UI 수정 v2.2
    UI바뀌면 말씀해주시기

--- FE/src/pages/MainPage.jsx
+++ FE/src/pages/MainPage.jsx
@@ -4,7 +4,8 @@ import ProjectListPage from "./project/ProjectListPage";
 const MainPage = () => {
   return (
     <>
-      <div style={{textAlign: 'center', width:'100%', height:'600px'}}>
+      <div>
+
         <ProjectListPage />
 
         <Link to='/project'>
