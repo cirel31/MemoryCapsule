af6360b949ad1288e0ad988523e0f23bbfa1c375
leejm9704@naver.com <leejm9704@naver.com>
2023-08-12 23:09:50 +0900

    S09P12A608-442] Design: ProjectDetail 메인 article 디자인 디테일 수정

--- FE/src/pages/project/ProjectDetailPage.jsx
+++ FE/src/pages/project/ProjectDetailPage.jsx
@@ -14,6 +14,8 @@ import "../../styles/ProjectDetailSytle.scss";
 import go_back from "../../assets/images/frield/go_back.svg";
 import write from "../../assets/images/projectdetail/write.svg";
 import discard from "../../assets/images/projectdetail/discard.svg";
+import before from "../../assets/images/projectdetail/before.svg";
+import after from "../../assets/images/projectdetail/after.svg";
 
 const baseURL = 'https://i9a608.p.ssafy.io:8000'
 const subURL = '/project'
@@ -113,7 +115,7 @@ const ProjectDetailPage = () => {
         <div className="detail_project_top"></div>
         <div className="detail_project_top_content">
           <div className="detail_project_title">
-            <h1>제목 : {project.title}</h1>
+            <h1>{project.title}</h1>
           </div>
           <div className="detail_project_back">
             <div onClick={handleBack} className="detail_project_back_button">
@@ -123,12 +125,21 @@ const ProjectDetailPage = () => {
         </div>
         <div className="detail_project_order">
           <div className="detail_project_info_logs">
-            <div className="detail_project_shorts_info_logs">
-              <p>현재까지 작성된 기록 : {myArticles.length} 개</p>
-              <p>오늘 작성된 기록 : {myArticles.length} 개</p>
+            <div className="detail_project_info_log">
+              <div className="detail_project_info_log_start">현재까지 작성된 기록</div>
+              <div className="detail_project_info_log_highlight"> {project.artielcNum} </div>
+              <div className="detail_project_info_log_end">개</div>
+              {
+                isSoloProject() &&
+                <>
+                  <div className="detail_project_info_log_start">내가 작성한 기록</div>
+                  <div className="detail_project_info_log_highlight">{myArticles.length}</div>
+                  <div className="detail_project_info_log_end">개</div>
+                </>
+              }
             </div>
             <div className="detail_project_shorts_info_percentage">
-              <p>진행도 : {myArticles.length} / {project.length} %</p>
+              <p>{(myArticles.length / project.artielcNum) * 100} %</p>
             </div>
             <div>
               <p>캡슐 제작 기간</p>
@@ -166,7 +177,10 @@ const ProjectDetailPage = () => {
         <div  className="detail_project_history">
           <div className="detail_project_history_title">
             <h2 className="">HISTORY</h2>
-            <span className="detail_project_history_subtitle">내가 쓴 글만 보여요!</span>
+            {
+              isSoloProject() &&
+              <span className="detail_project_history_subtitle">내가 쓴 글만 보여요!</span>
+            }
           </div>
           <div className="detail_project_history_format">
             {myArticles.map((article, idx) => (
@@ -175,17 +189,42 @@ const ProjectDetailPage = () => {
                       <h3 className="detail_project_history_title">{article.created.slice(2,4)}년 {article.created.slice(5, 7)}월 {article.created.slice(8, 10)}일</h3>
                   )}
                   <div className="detail_project_history_article">
+                    {/*수정 필요 현재 이미지 pos 따라서 좌우 버튼 생성*/}
+                    {
+                      article.images &&
+                      article.images.length <= 1
+                      ? <div></div>
+                      : <div className="detail_project_history_article_beforeafter_btn">
+                          <img src={before} alt="이전 사진" className="detail_project_history_article_beforeafter_img"/>
+                        </div>
+                    }
                     <div className="detail_project_history_article_imgbox">
                       {article.images ? (
                           <img src={`${article.images}`} alt="서버 이미지를 불러올 수 없습니다" className="detail_project_history_article_img"/>
                       ) : <img src={kokona} alt="클라이언트 이미지를 불러올 수 없습니다"  className="detail_project_history_article_img"/>
                       }
                     </div>
+                    {/*수정 필요 현재 이미지 pos 따라서 좌우 버튼 생성*/}
+                    {
+                      article.images &&
+                      article.images.length <= 1
+                          ? <div></div>
+                          : <div className="detail_project_history_article_beforeafter_btn">
+                            <img src={after} alt="다음 사진" className="detail_project_history_article_beforeafter_img"/>
+                          </div>
+                    }
                     <div className="detail_project_history_article_body">
                       {article.stamp && (
                           <img src={stamps[article.stamp - 1].stamp} alt="이미지를 불러올 수 없습니다" className="detail_project_history_article_stamp"/>
                       )}
-                      <p>{article.content}</p>
+                      <div className="detail_project_history_article_text_form">
+                        <p className="detail_project_history_article_text">{article.content}</p>
+                        {
+                          Array.from({length: 6}).map((_, index) => (
+                              <div key={index} className="detail_project_history_article_text_underline">{/*밑줄*/}</div>
+                          ))
+                        }
+                      </div>
                     </div>
                   </div>
                 </div>
