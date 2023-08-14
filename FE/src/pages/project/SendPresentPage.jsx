591e318b880cc260eb2a999201c6e948467782de
정도현 <cirel31@gmail.com>
2023-08-14 15:22:27 +0900

    [S09P12A608-527] Test: 카카오 공유 페이지 배포 파일 테스트

--- FE/src/pages/project/SendPresentPage.jsx
+++ FE/src/pages/project/SendPresentPage.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import {useEffect, useState} from "react";
 
 const SendPresentPage = () => {
+  const { Kakao } = window;
   const giftUrl = useParams()
   const baseURL = 'https://i9a608.p.ssafy.io:8000'
   const subURL = '/project/gift'
@@ -18,6 +19,16 @@ const SendPresentPage = () => {
         console.log(giftUrl)
       })
   }, [])
+
+  useEffect(() => {
+    if (!Kakao.isInitialized()) {
+      Kakao.init('1af0163235ced24b3f4bc66a23b24509');
+    }
+    Kakao.Share.createScrapButton({
+      container: '#kakao-share',
+      requestUrl: `https://i9a608.p.ssafy.io/project/present/${giftUrl.giftUrl}`,
+    })
+  }, []);
   
   return (
     <>
@@ -28,9 +39,23 @@ const SendPresentPage = () => {
         {datas.map((article) => (
           <div>
             <p>{article.content}</p>
+            
             <img src={article.images[0]} />
+            
+            {article.images.slice(1).map((image) => (
+              <img src={image} alt=""/>
+            ))}
+            
           </div>
         ))}
+      </div>
+      <div>
+        <button id="kakao-share">
+          <img
+            src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png"
+            alt="카카오링크 보내기 버튼"
+          />
+        </button>
       </div>
     </>
   )
